ce4acfe5e901525abf37ef5cedb425849633f5f2
lorena-kell <mrs.lorena.kell@gmail.com>
2022-09-19 11:31:57 -0500

    partial completion of intro to testing js completed

--- tests.js
+++ tests.js
@@ -1,15 +1,34 @@
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
-    it('should be a defined function', function() {
+    it("should be a defined function", function() {
       expect(typeof helloWorld).toBe('function');
     });
-    it('should return a string when called', function() {
+    it("should return a string when called", function() {
       expect(typeof helloWorld()).toBe("string");
     });
-    it('should return the string "Hello, World!" when executed', function() {
+    it("should return the string 'Hello, World!' when executed", function() {
       expect(helloWorld()).toBe("Hello, World!");
     });
     it("should never return 'undefined' when called", function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return "Hello, Jane!" when passed a "Jane" parameter', function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return "Hello, Alex!" only when passed an "Alex" parameter', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return "Hello, Pat!" only when passed an "Pat" parameter', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+});
+
